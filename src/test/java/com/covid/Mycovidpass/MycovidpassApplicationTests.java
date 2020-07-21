59e75e40c7ee637b085a4b85aa8b01963b145bd2
uday <you@gmail.com>
2020-07-22 00:20:54 +0530

    FIRST COMMIT

---
+++ src/test/java/com/covid/Mycovidpass/MycovidpassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid.Mycovidpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycovidpassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
